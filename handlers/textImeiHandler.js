60f85112ee054ce47ea49f0379658aa10b755eb5
Ahmad Taufik Aurahman <122547767+AhmadTaufik253@users.noreply.github.com>
2026-07-27 19:46:29 +0700

    update

--- handlers/textImeiHandler.js
+++ handlers/textImeiHandler.js
@@ -18,7 +18,21 @@ module.exports.process = async (socket, buf, hex) => {
     }
 
     socket.deviceImei = imei;
-    socket.write(Buffer.from('OK\r\n'));
+    // socket.write(Buffer.from('OK\r\n'));
+
+    // Coba tanpa \r\n
+    socket.write(Buffer.from('OK'));
+
+    // Atau coba echo balik length+IMEI yang sama persis (device kadang expect echo)
+    socket.write(buf);
+
+    // Atau coba format ack ala length-prefix juga (mirroring gaya packet asli)
+    const ackMsg = 'OK';
+    const ackBuf = Buffer.alloc(2 + ackMsg.length);
+    ackBuf.writeUInt16BE(ackMsg.length, 0);
+    ackBuf.write(ackMsg, 2, 'ascii');
+    socket.write(ackBuf);
+
     logger.info('TextImei: ACK dikirim: OK\\r\\n');
   } catch (err) {
     logger.error('TextImei handler error', err);
